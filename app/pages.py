b70a86471d25333671144f44348b4c3b1adef438
John R <johnlradford23@gmail.com>
2022-05-02 13:10:28 -0400

    Implemented fully working pagination.

--- app/pages.py
+++ app/pages.py
@@ -50,11 +50,14 @@ def home():
 		# Initialize empty messages list.
 		messages = []
 		for i in id_list:
-			typ, msg_data = imap.fetch(str(i), '(RFC822)')
-			for response_part in msg_data:
-				if isinstance(response_part, tuple):
-					msg = email.message_from_bytes(response_part[1])
-					messages.insert(0, [i, str(msg['from']), str(msg['subject']), str(msg['date'])])
+			try:
+				typ, msg_data = imap.fetch(str(i), '(RFC822)')
+				for response_part in msg_data:
+					if isinstance(response_part, tuple):
+						msg = email.message_from_bytes(response_part[1])
+						messages.insert(0, [i, str(msg['from']), str(msg['subject']), str(msg['date'])])
+			except:
+				print("Out of range, Will l3rn 2 cod3 2morrow!")
 
 		return messages
 
@@ -64,12 +67,12 @@ def home():
 			if isinstance(response_part, tuple):
 				msg = email.message_from_bytes(response_part[1])
 				if msg.get_content_type() == "text/plain":
-					return "From: " + str(msg['from']) + "\n" + "Subject: " + str(msg['subject']) + "\n\n" + msg.get_payload()
+					return "Index Number: " + str(msg_num) + "\n" + "From: " + str(msg['from']) + "\n" + "Subject: " + str(msg['subject']) + "\n\n" + msg.get_payload()
 				else:
 					for part in msg.walk():
 						print(part.get_content_type())
 						if part.get_content_type() == "text/plain":
-							return "From: " + str(msg['from']) + "\n" + "Subject: " + str(msg['subject']) + "\n\n" + str(part)
+							return "Index Number: " + str(msg_num) + "\n" + "From: " + str(msg['from']) + "\n" + "Subject: " + str(msg['subject']) + "\n\n" + str(part)
 	def get_id_list(imap):
 		# Select all mailbox information.
 		imap.select(str(folder), readonly=True)
@@ -92,11 +95,13 @@ def home():
 	imap.login(imap_user, imap_pass)
 	
 	# GET parameters
-	msg_num = request.args.get('msg_num')
+	msg_num = request.args.get('msg_num', type = int)
 	folder = request.args.get('folder')
-	page_num = request.args.get('page_num')
+	page_num = request.args.get('page_num', type = int)
 
 	# Handel if GET parameters are not supplied.
+	# num_msg_per_page Messages per page.
+	num_msg_per_page = 10
 
 	# If folder is not supplied,
 	if bool(folder) != True:
@@ -115,22 +120,30 @@ def home():
 		# And there are messages in the folder, 
 		id_list = get_id_list(imap)
 		if id_list != []:
-			# Default to the latest message in the box.
-			full_url = url_for('.home', msg_num=id_list[-1], **request.args)
+			# Default to the latest message on the page.
+			print("Page Num: " + str(page_num))
+			print("Most Recent Email ID: " + str(id_list[-1]))
+
+			top_msg_on_page = int(id_list[-1]) - (num_msg_per_page * page_num) + num_msg_per_page
+			print(top_msg_on_page)
+			full_url = url_for('.home', msg_num=top_msg_on_page, **request.args)
 			return redirect(full_url)
+			# Default to the latest message in the box.
+			#full_url = url_for('.home', msg_num=id_list[-1], **request.args)
+			#return redirect(full_url)
 
 
 	# If there are no messages in the folder,
 	if get_id_list(imap) == []:
 		messages = ""
 		body = "No messages in folder!"
+		num_pages = 1
 	else:
-		# 10 Messages per page.
-		last_msg_num = int(get_id_list(imap)[-1])
+		last_msg_num = int(get_id_list(imap)[-1]) + 1
 
 		# Find page number from msg_num.
 		difference = last_msg_num - int(msg_num)
-		correct_page_num = int(difference / 10) + 1
+		correct_page_num = int(difference / num_msg_per_page) + (difference % num_msg_per_page > 0)
 
 		# If not on the right page change pages,
 		if int(page_num) != correct_page_num:
@@ -142,55 +155,20 @@ def home():
 		# Set messages list based off of page_num and msg_num.
 
 		# Round up integer division to get number pages total.
-		num_pages = int(last_msg_num / 10) + (last_msg_num % 10 > 0)
+		num_pages = int(last_msg_num / num_msg_per_page) + (last_msg_num % num_msg_per_page > 0)
 
-		if int(page_num) == 1:
-			messages = message_list(range((last_msg_num - 10), last_msg_num))
-		if int(page_num) == 2:
-			messages = message_list(range((int(last_msg_num) - 10)))
-		if int(page_num) == 3:
-			messages = message_list(range((int(last_msg_num) - 10)))
+		#print("Number of pages: " + str(num_pages))
 
+		# Magic pagination algorithm. Do not touch!
+		#print(range((last_msg_num - (num_msg_per_page * page_num)), (last_msg_num - ((page_num - 1) * num_msg_per_page))))
+		messages = message_list(range((last_msg_num - (num_msg_per_page * page_num)), (last_msg_num - ((page_num - 1) * num_msg_per_page))))
 		body = get_msg_body(msg_num)
 
 # All messages on one page.
 #		messages = message_list(get_id_list(imap))
 #		body = get_msg_body(msg_num)
 
-
-#	if int(id_list[-1]) > 10:
-#		last_msg_of_page = (10 * int(page_num)) * -1
-#		if int(page_num) == 1:
-#			first_msg_of_page = -1
-#		else:
-#			first_msg_of_page = (last_msg_of_page - 10) * -1
-#		print("first_msg_of_page: " + str(first_msg_of_page))
-#		print("last_msg_of_page: " + str(last_msg_of_page))
-#		id_list = range(int(id_list[last_msg_of_page]), int(id_list[first_msg_of_page]))
-#		print(id_list)
-
-#	# If there are messages in the mailbox.
-#	if id_list:
-#		# If no GET parameter, do latest message.
-#		if msg_num == None:
-#			msg_num = id_list[-1]
-#			full_url = url_for('.home', msg_num=msg_num, **request.args)
-#			return redirect(full_url)
-#		if msg_num > id_list[-1]:
-#			msg_num = id_list[-1]
-#			full_url = url_for('.home', msg_num=msg_num, folder=folder)
-#			return redirect(full_url)
-#		
-#		body=get_msg_body(msg_num)
-#	else:
-#		if msg_num != None:
-#			msg_num = id_list[-1]
-#			full_url = url_for('.home', **request.args)
-#			return redirect(full_url)
-#		if msg_num == None:
-#			body="No message in folder!"
-#
-	return render_template("home.html", user=current_user, sorted_folders=sort_folders(imap), messages=messages, body=body)
+	return render_template("home.html", user=current_user, sorted_folders=sort_folders(imap), messages=messages, body=body, num_pages=num_pages)
 
 @pages.route("/send", methods=['GET', 'POST'])
 @login_required
